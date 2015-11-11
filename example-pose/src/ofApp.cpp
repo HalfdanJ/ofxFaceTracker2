c9c8ca9a9b06b63cd560c3515b17cb263f355267
Jonas Jongejan <jonas@halfdanj.dk>
2015-11-11 14:56:11 +0100

    pose example

--- example-pose/src/ofApp.cpp
+++ example-pose/src/ofApp.cpp
@@ -16,6 +16,20 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    // Draw the grabber
     grabber.draw(0,0);
+    
+    // Draw debug pose
     tracker.drawPose();
+    
+    // Apply the pose matrix
+    ofPushView();
+    ofPushMatrix();
+    tracker.applyPoseMatrix();
+    
+    // Draw sphere (0,0,0) is forehead
+    ofDrawSphere(0, 0, 150, 20);
+    
+    ofPopMatrix();
+    ofPopView();
 }
