10b4cd34875ea856ca2c30c4d3ad485e750af427
Sam Hains <sdh@eml.cc>
2018-04-18 18:18:03 -0400

    add c++ files

---
+++ example-facesubstitution/src/ofApp.cpp
@@ -0,0 +1,105 @@
+#include "ofApp.h"
+
+using namespace ofxCv;
+
+void ofApp::setup() {
+#ifdef TARGET_OSX
+	//ofSetDataPathRoot("../data/");
+#endif
+	ofSetVerticalSync(true);
+	cloneReady = false;
+	cam.initGrabber(1280, 720);
+	clone.setup(cam.getWidth(), cam.getHeight());
+	ofFbo::Settings settings;
+	settings.width = cam.getWidth();
+	settings.height = cam.getHeight();
+	maskFbo.allocate(settings);
+	srcFbo.allocate(settings);
+	camTracker.setup();
+	srcTracker.setup();
+	srcTracker.setIterations(25);
+	srcTracker.setAttempts(4);
+
+	faces.allowExt("jpg");
+	faces.allowExt("png");
+	faces.listDir("faces");
+	currentFace = 0;
+	if(faces.size()!=0){
+		loadFace(faces.getPath(currentFace));
+	}
+}
+
+void ofApp::update() {
+	cam.update();
+	if(cam.isFrameNew()) {
+		camTracker.update(toCv(cam));
+		
+		cloneReady = camTracker.getFound();
+		if(cloneReady) {
+			ofMesh camMesh = camTracker.getImageMesh();
+			camMesh.clearTexCoords();
+			camMesh.addTexCoords(srcPoints);
+			
+			maskFbo.begin();
+			ofClear(0, 255);
+			camMesh.draw();
+			maskFbo.end();
+			
+			srcFbo.begin();
+			ofClear(0, 255);
+			src.bind();
+			camMesh.draw();
+			src.unbind();
+			srcFbo.end();
+			
+			clone.setStrength(16);
+			clone.update(srcFbo.getTextureReference(), cam.getTextureReference(), maskFbo.getTextureReference());
+		}
+	}
+}
+
+void ofApp::draw() {
+	ofSetColor(255);
+	
+	if(src.getWidth() > 0 && cloneReady) {
+		clone.draw(0, 0);
+	} else {
+		cam.draw(0, 0);
+	}
+	
+	if(!camTracker.getFound()) {
+		drawHighlightString("camera face not found", 10, 10);
+	}
+	if(src.getWidth() == 0) {
+		drawHighlightString("drag an image here", 10, 30);
+	} else if(!srcTracker.getFound()) {
+		drawHighlightString("image face not found", 10, 30);
+	}
+}
+
+void ofApp::loadFace(string face){
+	src.loadImage(face);
+	if(src.getWidth() > 0) {
+		srcTracker.update(toCv(src));
+		srcPoints = srcTracker.getImagePoints();
+	}
+}
+
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+	loadFace(dragInfo.files[0]);
+}
+
+void ofApp::keyPressed(int key){
+	switch(key){
+	case OF_KEY_UP:
+		currentFace++;
+		break;
+	case OF_KEY_DOWN:
+		currentFace--;
+		break;
+	}
+	currentFace = ofClamp(currentFace,0,faces.size());
+	if(faces.size()!=0){
+		loadFace(faces.getPath(currentFace));
+	}
+}
---
+++ example-facesubstitution/src/ofApp.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCv.h"
+#include "Clone.h"
+#include "ofxFaceTracker.h"
+#include "ofxFaceTrackerThreaded.h"
+
+class ofApp : public ofBaseApp {
+public:
+	void setup();
+	void update();
+	void draw();
+	void dragEvent(ofDragInfo dragInfo);
+	void loadFace(string face);
+	
+	void keyPressed(int key);
+
+	ofxFaceTrackerThreaded camTracker;
+	ofVideoGrabber cam;
+	
+	ofxFaceTracker srcTracker;
+	ofImage src;
+	vector<ofVec2f> srcPoints;
+	
+	bool cloneReady;
+	Clone clone;
+	ofFbo srcFbo, maskFbo;
+
+	ofDirectory faces;
+	int currentFace;
+};
